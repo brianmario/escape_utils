105cd3eee8dc871ae9c5827840de4257f36088fd
Brian Lopez <seniorlopez@gmail.com>
2010-06-07 22:58:39 -0700

    some initial specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+# encoding: UTF-8
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/..')
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
+
+require 'escape_utils'
