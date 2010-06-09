309683b0c938e44ad50dde8b51d07b38734df58e
Brian Lopez <seniorlopez@gmail.com>
2010-06-08 18:39:53 -0700

    some specs for html unescaping

---
+++ spec/html/unescape_spec.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+
+describe EscapeUtils do
+  it "should respond to unescape_html" do
+    EscapeUtils.should respond_to(:unescape_html)
+  end
+
+  context "unescape_html" do
+    it "should unescape a basic html tag" do
+      EscapeUtils.unescape_html("&lt;some_tag/&gt;").should eql("<some_tag/>")
+    end
+
+    it "should unescape double-quotes" do
+      EscapeUtils.unescape_html("&lt;some_tag some_attr=&quot;some value&quot;/&gt;").should eql("<some_tag some_attr=\"some value\"/>")
+    end
+
+    it "should unescape single-quotes" do
+      EscapeUtils.unescape_html("&lt;some_tag some_attr=&#39;some value&#39;/&gt;").should eql("<some_tag some_attr='some value'/>")
+    end
+
+    it "should unescape the & character" do
+      EscapeUtils.unescape_html("&lt;b&gt;Bourbon &amp; Branch&lt;/b&gt;").should eql("<b>Bourbon & Branch</b>")
+    end
+  end
+end
