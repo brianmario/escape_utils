3ce1daf1ae2f40489ccfc56747e2f330c2f684dd
Brian Lopez <seniorlopez@gmail.com>
2010-06-11 21:52:05 -0700

    add javascript unescaping

--- ext/escape_utils.c
+++ ext/escape_utils.c
@@ -132,6 +132,42 @@ static size_t escape_javascript(unsigned char *out, const unsigned char *in, siz
   return total;
 }
 
+static size_t unescape_javascript(unsigned char *out, const unsigned char *in, size_t in_len) {
+  size_t total = 0;
+  unsigned char curChar;
+
+  total = in_len;
+  while (in_len) {
+    curChar = *in++;
+    if (curChar == '\\') {
+      if (*in == 'n') {
+        *out++ = '\n';
+        total--;
+      } else if (*in == '\\') {
+        *out++ = '\\';
+        total--;
+      } else if (*in == '\'') {
+        *out++ = '\'';
+        total--;
+      } else if (*in == '\"') {
+        *out++ = '\"';
+        total--;
+      } else if (*in == '/') {
+        *out++ = '/';
+        total--;
+      } else {
+        *out++ = curChar;
+      }
+      in++;
+    } else {
+      *out++ = curChar;
+    }
+    in_len--;
+  }
+
+  return total;
+}
+
 static VALUE rb_escape_html(VALUE self, VALUE str) {
   Check_Type(str, T_STRING);
 
@@ -241,6 +277,45 @@ static VALUE rb_escape_javascript(VALUE self, VALUE str) {
   return rb_output_buf;
 }
 
+static VALUE rb_unescape_javascript(VALUE self, VALUE str) {
+  if (str == Qnil) {
+    return rb_str_new2("");
+  }
+
+  Check_Type(str, T_STRING);
+
+  VALUE rb_output_buf;
+#ifdef HAVE_RUBY_ENCODING_H
+  rb_encoding *default_internal_enc = rb_default_internal_encoding();
+  rb_encoding *original_encoding = rb_enc_get(str);
+#endif
+  unsigned char *inBuf = (unsigned char*)RSTRING_PTR(str);
+  size_t len = RSTRING_LEN(str), new_len = 0;
+
+  // this is the max size the string could be
+  // TODO: we should try to be more intelligent about this
+  unsigned char *outBuf = (unsigned char *)malloc(sizeof(unsigned char *)*len);
+
+  // perform our escape, returning the new string's length
+  new_len = unescape_javascript(outBuf, inBuf, len);
+
+  // create our new ruby string
+  rb_output_buf = rb_str_new((char *)outBuf, new_len);
+
+  // free the temporary C string
+  free(outBuf);
+
+#ifdef HAVE_RUBY_ENCODING_H
+  rb_enc_associate(rb_output_buf, original_encoding);
+  if (default_internal_enc) {
+    rb_output_buf = rb_str_export_to_enc(rb_output_buf, default_internal_enc);
+  } else {
+    rb_output_buf = rb_str_export_to_enc(rb_output_buf, utf8Encoding);
+  }
+#endif
+  return rb_output_buf;
+}
+
 /* Ruby Extension initializer */
 void Init_escape_utils_ext() {
   VALUE mEscape = rb_define_module("EscapeUtils");
@@ -250,6 +325,8 @@ void Init_escape_utils_ext() {
   rb_define_module_function(mEscape,  "unescape_html",  rb_unescape_html, 1);
   rb_define_method(mEscape,           "escape_javascript",  rb_escape_javascript, 1);
   rb_define_module_function(mEscape,  "escape_javascript",  rb_escape_javascript, 1);
+  rb_define_method(mEscape,           "unescape_javascript",  rb_unescape_javascript, 1);
+  rb_define_module_function(mEscape,  "unescape_javascript",  rb_unescape_javascript, 1);
 
 #ifdef HAVE_RUBY_ENCODING_H
   utf8Encoding = rb_utf8_encoding();
---
+++ spec/javascript/unescape_spec.rb
@@ -0,0 +1,39 @@
+# encoding: UTF-8
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+
+describe EscapeUtils, "unescape_javascript" do
+  it "should respond to unescape_javascript" do
+    EscapeUtils.should respond_to(:unescape_javascript)
+  end
+
+  # these are from the ActionView tests
+  it "should return an empty string if passed nil" do
+    EscapeUtils.unescape_javascript(nil).should eql("")
+  end
+
+  it "should unescape quotes and newlines" do
+    EscapeUtils.unescape_javascript(%(This \\"thing\\" is really\\n netos\\n\\n\\')).should eql(%(This "thing" is really\n netos\n\n'))
+  end
+
+  it "should unescape backslashes" do
+    EscapeUtils.unescape_javascript(%(backslash\\\\test)).should eql(%(backslash\\test))
+  end
+
+  it "should unescape closed html tags" do
+    EscapeUtils.unescape_javascript(%(dont <\\/close> tags)).should eql(%(dont </close> tags))
+  end
+
+  if RUBY_VERSION =~ /^1.9/
+    it "should default to utf-8 if Encoding.default_internal is nil" do
+      Encoding.default_internal = nil
+      EscapeUtils.unescape_javascript(%(dont <\\/close> tags)).encoding.should eql(Encoding.find('utf-8'))
+    end
+
+    it "should use Encoding.default_internal" do
+      Encoding.default_internal = Encoding.find('utf-8')
+      EscapeUtils.unescape_javascript(%(dont <\\/close> tags)).encoding.should eql(Encoding.default_internal)
+      Encoding.default_internal = Encoding.find('us-ascii')
+      EscapeUtils.unescape_javascript(%(dont <\\/close> tags)).encoding.should eql(Encoding.default_internal)
+    end
+  end
+end
