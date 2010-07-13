41c5d5d034dffdba2f14463cbad43597c69b6ca9
Brian Lopez <seniorlopez@gmail.com>
2010-07-13 09:21:07 -0700

    add overrides for url unescaping in Rack and CGI

--- lib/escape_utils/url/cgi.rb
+++ lib/escape_utils/url/cgi.rb
@@ -4,4 +4,7 @@ class CGI
   def self.escape(s)
     EscapeUtils.escape_url(s)
   end
+  def self.unescape(s)
+    EscapeUtils.unescape_url(s)
+  end
 end
--- lib/escape_utils/url/rack.rb
+++ lib/escape_utils/url/rack.rb
@@ -5,6 +5,10 @@ module Rack
     def escape(url)
       EscapeUtils.escape_url(url)
     end
+    def unescape(url)
+      EscapeUtils.unescape_url(url)
+    end
     module_function :escape
+    module_function :unescape
   end
 end
