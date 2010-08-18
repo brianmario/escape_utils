051efbca6173ca35a530e391aabdea876b9b2708
Brian Lopez <seniorlopez@gmail.com>
2010-08-18 15:15:39 -0700

    ...

--- lib/escape_utils/url/cgi.rb
+++ lib/escape_utils/url/cgi.rb
@@ -2,9 +2,9 @@
 
 class CGI
   def self.escape(s)
-    EscapeUtils.variable_position(s.to_s)
+    EscapeUtils.escape_url(s.to_s)
   end
   def self.unescape(s)
-    EscapeUtils.unvariable_position(s.to_s)
+    EscapeUtils.unescape_url(s.to_s)
   end
 end
