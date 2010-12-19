2d3e521cd2a21adbed3e60365ded427f459335fa
Brian Lopez <seniorlopez@gmail.com>
2010-12-18 17:05:10 -0800

    move to rspec2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/..')
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 
+require 'rspec'
 require 'escape_utils'
