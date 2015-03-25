7eefc9e18a720e5d0365ceb89d831a43557a04e6
Patrick Toomey <ptoomey3@biasedcoin.com>
2015-03-25 11:13:21 -0600

    fix typos and add additional tests

--- test/uri/unescape_test.rb
+++ test/uri/unescape_test.rb
@@ -30,18 +30,29 @@ class UriUnescapeTest < Minitest::Test
     assert_equal matz_name_sep, EscapeUtils.unescape_uri('%E3%81%BE%E3%81%A4%20%E3%82%82%E3%81%A8')
   end
 
+  def test_uri_containing_pluses
+    assert_equal "a+plus", EscapeUtils.unescape_uri("a%2Bplus")
+  end
+
+  def test_escape_unescape_roundtrip
+    (0..127).each do |index|
+      char = index.chr
+      assert_equal char, EscapeUtils.unescape_uri(EscapeUtils.escape_uri(char))
+    end
+  end
+
   if RUBY_VERSION =~ /^1.9/
     def test_input_must_be_valid_utf8_or_ascii
       escaped = EscapeUtils.escape_uri("fo<o>bar")
 
       escaped.force_encoding 'ISO-8859-1'
       assert_raises Encoding::CompatibilityError do
-        EscapeUtils.unescape_url(escaped)
+        EscapeUtils.unescape_uri(escaped)
       end
 
       escaped.force_encoding 'UTF-8'
       begin
-        EscapeUtils.unescape_url(escaped)
+        EscapeUtils.unescape_uri(escaped)
       rescue Encoding::CompatibilityError => e
         assert_nil e, "#{e.class.name} raised, expected not to"
       end
@@ -49,7 +60,7 @@ class UriUnescapeTest < Minitest::Test
 
     def test_return_value_is_tagged_as_utf8
       escaped = EscapeUtils.escape_uri("a space")
-      assert_equal Encoding.find('UTF-8'), EscapeUtils.unescape_url(escaped).encoding
+      assert_equal Encoding.find('UTF-8'), EscapeUtils.unescape_uri(escaped).encoding
     end
   end
 end
